9e502df6bfb6067e079b73c3c14863c66806d47b
Michael Naghavipour <m.naghavipour@reply.de>
2025-05-06 00:43:54 +0200

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+console.log('Starting index.js');
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
 import { createServer, startServer } from './server';
 
 const app = createServer();
